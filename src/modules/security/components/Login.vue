87e99a42dc00d9d5264c92c4c66d149345e6a418
zhangkun <1766597067@qq.com>
2018-05-31 14:18:18 +0800

    登录

--- src/modules/security/components/Login.vue
+++ src/modules/security/components/Login.vue
@@ -1,16 +1,16 @@
 <template>
-  <section class="form">
+  <section class="form login-wrapper">
+    <div class="form-title">登录</div>
     <div class="form-group" :class="{'error': error.name}">
-      <label class="form-title">账号</label>
+      <label class="form-label">账号</label>
       <input class="form-control" type="text" placeholder="账号"/>
     </div>
     <zk-jitter :start.sync="begin" class="form-group" :class="{'error': error.pwd}">
-      <label class="form-title">密码</label>
+      <label class="form-label">密码</label>
       <input class="form-control" type="password" placeholder="密码"/>
       <p class="tip">请输入秘密</p>
     </zk-jitter>
-    <zk-button @click="jitter" class="btn btn-blue w-100 mt-10 fs-14">按钮</zk-button>
-    <zk-button @click="jittera" class="btn btn-blue w-100 mt-10 fs-14">取消</zk-button>
+    <zk-button @click="jitter" class="btn btn-blue btn-md w-100 mt-10">按钮</zk-button>
   </section>
 </template>
 
@@ -39,5 +39,8 @@
 </script>
 
 <style lang="scss">
-
+  .login-wrapper{
+    width: 400px;
+    margin-top: 15vh;
+  }
 </style>
